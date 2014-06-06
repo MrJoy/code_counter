3ef6aacc0867fb54617e1a1976ea7e8563957368
Jon Frisby <jfrisby@mrjoy.com>
2014-06-05 23:52:40 -0700

    Flesh out test a bit.

--- spec/fixtures/syntax_corner_cases/lib/multiple_classes_in_file.rb
+++ spec/fixtures/syntax_corner_cases/lib/multiple_classes_in_file.rb
@@ -1,4 +1,6 @@
 class FirstClassInFile
+  # Note the trailing whitespace on the next line:
+    
 end
 
 class SecondClassInFile < Foo; def bar; 1; end; end
---
+++ spec/fixtures/syntax_corner_cases/lib/not_actually_a_single_line_class.rb
@@ -0,0 +1,40 @@
+=begin
+While we're at it, let's have a go at a multi-line comment!
+class Bogosity
+  def wheee
+    dhsjhdsd
+    asdas
+    dsd
+    ad
+    asd
+    sa
+  end
+  def wheee
+    dhsjhdsd
+    asdas
+    dsd
+    ad
+    asd
+    sa
+  end
+  def wheee
+    dhsjhdsd
+    asdas
+    dsd
+    ad
+    asd
+    sa
+  end
+  def wheee
+    dhsjhdsd
+    asdas
+    dsd
+    ad
+    asd
+    sa
+  end
+end
+=end
+class SingleLineClass < Foo#; def bar; 1; end; end
+  def bar; 1; end
+end
