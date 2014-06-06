849f7da4680b53bfbc0a051cb10433caa88c0ce0
Jon Frisby <jfrisby@mrjoy.com>
2014-06-06 04:36:41 -0700

    A few more corner cases...

--- spec/fixtures/syntax_corner_cases/lib/nested_classes_and_code_outside_methods.rb
+++ spec/fixtures/syntax_corner_cases/lib/nested_classes_and_code_outside_methods.rb
@@ -1,15 +1,37 @@
-code_outside_class
+code_outside_class :a
+flippity floppy boop
 
 def method_outside_class
   intra_method_statement
+rescue SomeError => se
+rescue SomeOtherError
+rescue YAError1, YAError2
+rescue YAError3, YAError4 => yae
+ensure
+  whatever
 end
 
-class Foo
+class Foo < Meh
   intra_class_statement
 
   def foo
     intra_method_statement
+
+    class WibblyWobbly
+    end
   end
+
+  protected
+
+  def self.self_bar; end
+
+  def Foo.self_foo; end
+
+  def bar; end
+
+  private def baz; puts "OY"; end
+
+  public
 
   class Bar
     more_intra_class_statements
@@ -18,4 +40,17 @@ class Foo
       intra_method_statement
     end
   end
+end
+
+class A::B
+end
+
+module Alpha
+  module Beta
+    class Delta::Gamma
+    end
+  end
+end
+
+module One::Two
 end
