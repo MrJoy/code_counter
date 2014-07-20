a1f467f9358708fba4e44fb2458a7764a001a47d
Jon Frisby <jfrisby@mrjoy.com>
2014-07-20 16:37:16 -0700

    Note to self.

--- lib/code_counter/engine.rb
+++ lib/code_counter/engine.rb
@@ -225,6 +225,8 @@ module CodeCounter
       type_loc
     end
 
+    # TODO: Make this respond to changes caused by `.add_path` and
+    # TODO: `.add_test_group`.
     COL_WIDTHS      = [[22,-1], [7,1], [7,1], [9,1], [9,1], [5,1], [7,1]]
     HEADER_PATTERN  = '|' + COL_WIDTHS.map { |(w,_)| "%-#{w}s" }.join('|') + "|\n"
     ROW_PATTERN     = '|' + COL_WIDTHS.map { |(w,d)| "%#{w*d}s" }.join('|') + "|\n"
